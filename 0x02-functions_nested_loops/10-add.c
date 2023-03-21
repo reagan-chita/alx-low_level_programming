59a620697faac028b3e6475f9e5ca7b9098b868e
reagan-chita <reaganchita@gmail.com>
2023-03-21 11:06:15 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,28 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers
- * from n to 98.
+ * add - Adds two integers and returns
+ * the result.
  *
- * @n: input number.
+ * @a: number one.
+ * @b: number two.
  *
- * Return: no return.
+ * Return: Add of number one and number two.
  */
-void print_to_98(int n)
+int add(int a, int b)
 {
-	if (n > 98)
-	{
-		for (; n > 98; n--)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
+	return (a + b);
 }
