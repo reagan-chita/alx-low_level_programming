4ff618042094f08df76f633e8d86103841469fc9
reagan-chita <reaganchita@gmail.com>
2023-03-21 10:08:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _main_h_
+#define _main_h_
 
-/* Include any necessary header files here */
-#include <stdio.h>
-
-/* Function prototypes */
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
-int main(int argc, char *argv[]);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-#endif /* MAIN_H */
-
+#endif
