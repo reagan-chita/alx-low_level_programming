4a53b5d644c4178b728a6a75ced731577963763d
reagan-chita <reaganchita@gmail.com>
2023-03-21 07:51:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Include any necessary header files here */
+#include <stdio.h>
+
+/* Function prototypes */
+int _putchar(char c);
+void print_alphabet(void);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
+
