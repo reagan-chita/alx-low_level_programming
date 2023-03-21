248d137b39d4503e2f3b8698667be6b27c8605fe
reagan-chita <62418663+reagan-chita@users.noreply.github.com>
2023-03-21 16:09:16 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
-#include "main.h"
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
- *print_alphabet - Prints the alphabet in lowercase.
- *
- *Return: Always 0 (Success)
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	int i;
+	char letter;
 
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 	_putchar('\n');
 }
